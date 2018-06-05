e151e0f9a6cba0d3b43a324ef7708a84669895a2
Dave Newson <dave.newson@elmolearning.com.au>
2018-06-05 14:54:48 +1000

    Added chainable calls to the "HeaderBar" to make usage simpler.

---
+++ test/sample/header-bar.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const term = require('terminal-kit').terminal;
+require('../../index.js').plugin(term);
+
+term.HeaderBar({
+    x: 0,
+    y: 0,
+    width: null, // full width
+    style: term.bgBrightYellow.black,
+    divider: "|",
+    dividerStyle: term.bgBrightYellow.brightRed,
+    padding: 4,
+    get: "label",
+    getKey: "key",
+})
+    .add({ label: "Alpha", key: "a" })
+    .add({ label: "Bravo", key: "b" })
+    .on('selected', (item) => {
+        console.log("Item: " + item.label);
+        term.processExit();
+    })
+    .redraw();
+
+// Wait for input (header does not cause this)
+term.grabInput();

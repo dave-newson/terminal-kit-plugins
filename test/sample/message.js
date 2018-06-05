db2d05642764a532fe2ac6fc4c78c4c75f4553ab
Dave Newson <dave.newson@elmolearning.com.au>
2018-06-05 14:53:29 +1000

    Improved "Message" element to support custom styles and aliases.
    Default styles updated to be more in-line with friendly CLI tools (prefix, padding, colours)

---
+++ test/sample/message.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const term = require('terminal-kit').terminal;
+require('../../index.js').plugin(term);
+
+// Standard message types
+term.Message('Default message');
+
+term.Message('Error message', 'error');
+
+term.Message('Warning message', 'warning');
+
+term.Message('Notice message', 'notice');
+
+term.Message('Success message', 'success');
+
+term.Message('Standard message', 'standard');
+
+// Note: data does not incorporate its own new lines
+term.noFormat('\n');
+const msg = term.Message();
+msg.show('ThisShouldAppearAs', 'data');
+msg.show('OneSingleLine', 'data');
+term.noFormat('\n');
+
+// Custom message design
+term.Message().addType('alias', {
+    prefix: '[TEST]',
+    padding: 2,
+    full_width: true,
+    style: ['blink', 'brightRed', 'bgBlue'],
+});
+
+term.Message('Customised message', 'alias');

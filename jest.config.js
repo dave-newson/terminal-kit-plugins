f8644b3ff423fd5a235f6519a8b39654626d4383
Dave Newson <dave.newson@elmosoftware.com.au>
2026-03-18 10:52:04 +1100

    Fix: map chroma-js to CJS build in jest to handle ESM incompatibility with Jest v24

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
     transform: {'^.+\\.ts?$': 'ts-jest'},
     testEnvironment: 'node',
     testRegex: '/src/.*\\.(test|spec)?\\.(ts|tsx)$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    moduleNameMapper: {
+        '^chroma-js$': '<rootDir>/node_modules/chroma-js/dist/chroma.cjs',
+    },
 };
